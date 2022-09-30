bb785555ffd7fcf9cad3a284fcd3a85385cfc6b0
Valentin <kueck.valentin@googlemail.com>
2022-09-30 13:41:48 +0200

    add surrealdb connection

--- watcher.ts
+++ watcher.ts
@@ -1,16 +1,45 @@
+import Surreal from "https://deno.land/x/surrealdb@v0.2.0/mod.ts";
+import { parse } from "https://deno.land/x/xml/mod.ts";
+
+const db = new Surreal('http://127.0.0.1:8000/rpc', "");
+
+try {
+
+    // Signin as a namespace, database, or root user
+    await db.signin({
+        user: 'root',
+        pass: 'root',
+    });
+
+    // Select a specific namespace / database
+    await db.use('test', 'test');
+
+
+
+} catch (e) {
+
+    console.error('ERROR', e);
+
+}
+
+
 
 const watcher = Deno.watchFs("./");
 
 for await (const event of watcher) {
     console.log(">>>> event", event);
     console.log(event.paths);
-    const decoder = new TextDecoder("utf-8");
     for (const path of event.paths) {
-        const file = await Deno.readFile(path)
-        console.log(decoder.decode(file));
+        const xml = parse(await Deno.open(path));
+        console.log(xml);
+        db.create("alert", xml.alert)
     }
 
 
 
     console.log(new Date());
 }
+
+
+
+
