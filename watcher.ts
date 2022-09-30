6f5b2d6d03e614584c9744c28a93468728671955
Valentin <kueck.valentin@googlemail.com>
2022-09-30 13:02:21 +0200

    first commit

---
+++ watcher.ts
@@ -0,0 +1,6 @@
+
+const watcher = Deno.watchFs("./");
+for await (const event of watcher) {
+    console.log(">>>> event", event);
+    // Example event: { kind: "create", paths: [ "/home/alice/deno/foo.txt" ] }
+}
