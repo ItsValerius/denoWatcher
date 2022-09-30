d01b52db445c3e74ae1641f14246acc61128331c
Valentin <kueck.valentin@googlemail.com>
2022-09-30 13:34:59 +0200

    readfile

--- watcher.ts
+++ watcher.ts
@@ -4,8 +4,10 @@ const watcher = Deno.watchFs("./");
 for await (const event of watcher) {
     console.log(">>>> event", event);
     console.log(event.paths);
-    const file = await Deno.open(event.paths[0])
-    console.log(file);
+    const decoder = new TextDecoder("utf-8");
+    const file = await Deno.readFile(event.paths[0])
+    console.log(decoder.decode(file));
+
 
     console.log(new Date());
 }
