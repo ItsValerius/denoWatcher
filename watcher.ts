d1bf4cfb897057e0b8c7dc6d1b6166f564aa8e3e
Valentin <kueck.valentin@googlemail.com>
2022-09-30 13:25:13 +0200

    add time

--- watcher.ts
+++ watcher.ts
@@ -1,6 +1,7 @@
 
 const watcher = Deno.watchFs("./");
+
 for await (const event of watcher) {
     console.log(">>>> event", event);
-    // Example event: { kind: "create", paths: [ "/home/alice/deno/foo.txt" ] }
+    console.log(new Date());
 }
