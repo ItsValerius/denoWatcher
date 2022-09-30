77fec147578ae07e69e4c18cb19feb13d8c80a51
Valentin <kueck.valentin@googlemail.com>
2022-09-30 13:32:22 +0200

    open file

--- watcher.ts
+++ watcher.ts
@@ -4,6 +4,8 @@ const watcher = Deno.watchFs("./");
 for await (const event of watcher) {
     console.log(">>>> event", event);
     console.log(event.paths);
+    const file = await Deno.open(event.paths[0])
+    console.log(file);
 
     console.log(new Date());
 }
